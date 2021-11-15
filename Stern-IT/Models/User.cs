b1e110fbf4bacf008f73debdb9669362eef47583
Ido Sternheim <ido8447@gmail.com>
2021-11-15 23:50:39 +0200

    commit

---
+++ Stern-IT/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stern_IT.Models
+{
+    public class User : IdentityUser
+    {
+    }
+}
--- Stern-IT/Startup.cs
+++ Stern-IT/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Stern_IT.Models;
 
 namespace Stern_IT
 {
@@ -20,6 +22,8 @@ namespace Stern_IT
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Connect to PostgreSQL
+            services.AddDbContext<Models.DbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
