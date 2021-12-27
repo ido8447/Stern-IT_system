7c127f19e96aba6c55fff98398275c63da9be77e
Ido Sternheim <ido8447@gmail.com>
2021-12-27 13:51:53 +0200

    show answers on details page

--- Stern-IT/ClientApp/src/app/ticket/components/details-of-ticket/details-of-ticket.component.ts
+++ Stern-IT/ClientApp/src/app/ticket/components/details-of-ticket/details-of-ticket.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Answer } from "src/app/models/Answer";
 import { Ticket } from "src/app/models/ticket.model";
 import { TicketService } from "src/app/services/ticket.service";
 import { UserService } from "src/app/services/user.service";
@@ -11,13 +12,16 @@ import { UserService } from "src/app/services/user.service";
 })
 export class DetailsOfTicketComponent implements OnInit {
   public ticket: Ticket;
+  modelList: Array<Answer>;
+
   /**
    *
    */
   constructor(
     private router: Router,
     private activedRoute: ActivatedRoute,
-    private ticketService: TicketService
+    private ticketService: TicketService,
+    private userService: UserService,
   ) {}
   ngOnInit() {
     const id = parseInt(this.activedRoute.snapshot.paramMap.get("id"));
@@ -29,11 +33,27 @@ export class DetailsOfTicketComponent implements OnInit {
         console.log(error);
       }
     );
+    this.GetAnswer(id.toString());
+  
   }
+
 
   public cancel() {
     this.router.navigateByUrl("/tickets");
   }
 
+  GetAnswer(ID: string) {
+    this.ticketService
+      .GetAnswer(ID)
+      .subscribe((arg) => (this.modelList = arg as Answer[]
+      ));
+  }
+
+
+  formTicketModel = {
+    Email: this.userService.getAuthorizedUserEmail(),
+    Answer: "",
+    TicketId: parseInt(this.activedRoute.snapshot.paramMap.get("id")),
+  };
   
 }
